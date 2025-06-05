17ffac855aa99fdfcb296907bba1c2db6fc2cc57
Faijur-Rahman <faijurrahman3245@gmail.com>
2025-06-05 09:15:31 +0530

    optimized

--- src/main/java/com/todo/project/controller/TodoController.java
+++ src/main/java/com/todo/project/controller/TodoController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = {"http://localhost:8080", "http://127.0.0.1:5500", "null"})
 public class TodoController {
 
     @Autowired
